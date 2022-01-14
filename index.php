357991aaa1bdc51f2446db76d5de9b4304529b09
DANIELA MORA <DANIELA.MORA@SEGUROSNOVUS.COM.CO>
2022-01-14 16:27:05 -0500

    file creation

---
+++ index.php
@@ -0,0 +1,52 @@
+<!Doctype HTML>
+       <html lang="es-ES">
+       <head>
+       <head>
+         <meta charset="utf-8" />
+         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+         <meta name="description" content="" />
+         <meta name="author" content="" />
+       <link
+       href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css"
+       rel="stylesheet"
+       integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl"
+       crossorigin="anonymous">
+     
+       <link rel="icon" type="image/png" sizes="16x16" href="../../../images/favicon.png">
+            <link rel="stylesheet" type="text/css" href="../../../css/flaticon.css" >
+            <link rel="stylesheet" type="text/css" href="../../../css/font-awesome-old/css/font-awesome.min.css" >
+           <meta name="viewport" content="width=device-width, initial-scale=1">
+       <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10.10.1/dist/sweetalert2.all.min.js"></script>
+       <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+       <title>| Actualizar usuario</title>
+       </head>
+       <body>
+       <!--<script> 
+        window.addEventListener("load", init, false);
+            function init () {
+                Swal.fire({
+                    title: "¡Excelente!",
+                    text: "Usuario actualizado,Por favor inicie sesión nuevamente para finalizar el proceso!",
+                    icon: "success",
+                    buttons: true,
+                    dangerMode: true,
+                  }).then((willDelete) => {
+                if (willDelete) {
+                    location.href = "swal.php";
+                } else {
+                    location.href = "swal.php";
+                }
+              });
+            }
+            
+       </script>-->
+       <?php
+       echo ('<script>Swal.fire(
+                            "Error!!",
+                            "Datos inválidos",
+                            "error"
+                             )</script>');
+    ?>
+       
+       </body>
+       </html>
