ec4a5e08f5c81786958f0fb5a3dbbefb2a17d003
DANIELA MORA <DANIELA.MORA@SEGUROSNOVUS.COM.CO>
2022-01-19 08:48:56 -0500

    frontend fix at login

--- index.php
+++ index.php
@@ -1,20 +1,45 @@
-<?php
-    include "config.php"; 
-?>
+<?php include "config.php"; ?>
 
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="<?=URL_CSS ?>styleIndex.css"/>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10.10.1/dist/sweetalert2.all.min.js"></script>
-    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-    <title>Login || Sign-in</title>
+    <head>
+        <?php include FOLDER_TEMPLATE . 'headLogin.php' ?>
     </head>
+
     <body>
-        <form id="Form" action="<?= URL_CONTROLLER ?>User-controller.php" method="POST">
+    <div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+				<form id="Form" action="<?= URL_CONTROLLER ?>User-controller.php" method="POST" class="login100-form validate-form">
+					<span class="login100-form-title p-b-26">Welcome</span>
+					<!--<span class="login100-form-title p-b-48"><i class="zmdi zmdi-font"></i></span>-->
+
+					<div class="wrap-input100 validate-input">
+						<input class="input100" type="number" name="Document" id="Document" required>
+						<span class="focus-input100" data-placeholder="Document"></span>
+					</div>
+
+					<div class="wrap-input100 validate-input">
+						<span class="btn-show-pass"><i class="zmdi zmdi-eye"></i></span>
+						<input class="input100" type="password" name="Pass" id="Pass" required>
+						<span class="focus-input100" data-placeholder="Password"></span>
+					</div>
+
+					<div class="container-login100-form-btn">
+						<div class="wrap-login100-form-btn">
+							<div class="login100-form-bgbtn"></div>
+							<button class="login100-form-btn" name="submit" id="submit">Login</button>
+						</div>
+					</div>
+						<!--<a class="txt2" href="#">Sign Up </a>-->
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	<div id="dropDownSelect1"></div>
+
+        <!--<form id="Form" action="<?= URL_CONTROLLER ?>User-controller.php" method="POST">
             <h1 class="exampleone">Sign-in</h1><br/>
             <div class="mb-3">
                 <label class="form-label">Document <input type="number" class="form-control" id="Document" name="Document" required/></label>
@@ -23,6 +48,10 @@
                 <label class="form-label">Password <input class="form-control" type="password" id="Pass" name="Pass" required/></label>
             </div>
                 <button type="submit" class="btn btn-primary" name="submit" id="submit" name="submit">Submit</button>
-        </form>
+        </form>-->
+        <?php include FOLDER_TEMPLATE . 'scriptsLogin.php' ?>
     </body>
     </html>
+
+
+    
---
+++ template/scriptsLogin.php
@@ -0,0 +1,11 @@
+<script src="assets/vendor/jquery/jquery-3.2.1.min.js"></script>
+<script src="assets/vendor/animsition/js/animsition.min.js"></script>
+<script src="assets/vendor/bootstrap/js/popper.js"></script>
+<script src="assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+<script src="assets/vendor/select2/select2.min.js"></script>
+<script src="assets/vendor/daterangepicker/moment.min.js"></script>
+<script src="assets/vendor/daterangepicker/daterangepicker.js"></script>
+<script src="assets/vendor/countdowntime/countdowntime.js"></script>
+<script src="assets/js/main.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10.10.1/dist/sweetalert2.all.min.js"></script>
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
